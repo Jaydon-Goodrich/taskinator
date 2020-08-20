139c7fbcd3857161f6607ee697ff835a657cf8b4
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-08-20 00:18:23 -0600

    finished delete task function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 
 var taskFormHandler = function(event){
@@ -92,5 +93,18 @@ var createTaskActions = function(taskId) {
 
     return actionContainerEl;
 }
+var taskButtonHandler = function(event) {
 
+    if(event.target.matches(".delete-btn")){
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 formEl.addEventListener("submit", taskFormHandler);
